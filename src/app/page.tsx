2d43376428fd4db657b41123c08b094b62e50503
c3i-vipuls <vipuls@c3ihub.iitk.ac.in>
2026-07-25 15:56:13 +0530

    Init

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-semibold tracking-tight">Hello World</h1>
+    </main>
+  );
+}
